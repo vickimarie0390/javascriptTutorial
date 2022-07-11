fc432dc8b0fd549277af4e1fc9d1a564f1c5550c
Victoria <vickimarji@icloud.com>
2022-07-10 20:24:30 -0400

    please work

---
+++ index.js
@@ -0,0 +1,22 @@
+var myVariableVictoria;
+myVariableVictoria = 10;
+
+var myVar2 = "hello";
+myVar2
+myVar2 = "hello again";
+myVar2
+myVar2 = 5;
+var customerAge = 32;
+customerAge;
+var myVar3 = 40;
+
+5 + 5
+myVar2 + 10
+20 / 20
+myVar2 = myVar2 / 2
+
+myVar2 += 5
+myVar2 /+ 2
+var myVar2 = "Victoria"
+document.write(myVar2)
+console.log(myVar2)
